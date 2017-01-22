025edf543efe0e4ff05f3cf978aa496d82c45737
Albert Qiu <260381980@qq.com>
2017-01-22 14:00:48 +0800

    添加代码

---
+++ caches/configs/database.php
@@ -0,0 +1,19 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => '127.0.0.1',
+		'port' => 3306,
+		'database' => 'phpcmsv9',
+		'username' => 'albert',
+		'password' => 'allies2013',
+		'tablepre' => 'v9_',
+		'charset' => 'utf8',
+		'type' => 'mysqli',
+		'debug' => true,
+		'pconnect' => 0,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ caches/error_log.php
@@ -0,0 +1,7 @@
+<?php exit;?>01-22 12:35:23 | 2 | mysqli_connect(): (HY000/2002): No such file or directory | /var/www/phpcms/install/install.php | 353
+<?php exit;?>01-22 12:40:01 | 2 | file_get_contents(http://www.phpcms.cn/upgrademd5/20151225_utf-8.php): failed to open stream: HTTP request failed! HTTP/1.1 404 Not Found
+ | phpcms/modules/upgrade/index.php | 187
+<?php exit;?>01-22 12:40:01 | 2 | array_diff(): Argument #1 is not an array | phpcms/modules/upgrade/index.php | 190
+<?php exit;?>01-22 12:40:01 | 2 | Invalid argument supplied for foreach() | phpcms/modules/upgrade/index.php | 194
+<?php exit;?>01-22 12:40:01 | 2 | array_keys() expects parameter 1 to be array, null given | phpcms/modules/upgrade/index.php | 202
+<?php exit;?>01-22 12:40:01 | 2 | array_diff(): Argument #2 is not an array | phpcms/modules/upgrade/index.php | 202
